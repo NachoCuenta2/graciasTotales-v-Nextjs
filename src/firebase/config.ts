207f6cc32d6325d7f74ecc04317d5e027d1f18a2
Nacho Trevisan <nachotizii988@gmail.com>
2024-11-06 09:06:07 -0300

    quite las credenciales de firebase

---
+++ src/firebase/config.ts
@@ -0,0 +1,17 @@
+// firebase/config.ts
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+};
+
+export const FireBaseApp = initializeApp(firebaseConfig);
+export const FireBaseAuth = getAuth(FireBaseApp);
+export const FireBaseDB = getFirestore(FireBaseApp);
