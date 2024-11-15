bc017c84a697d99225d9da42e3b4911c77041585
Nacho Trevisan <nachotizii988@gmail.com>
2024-11-15 16:15:23 -0300

    api remoto agregado

--- src/config/gracias-totales-fetcher.ts
+++ src/config/gracias-totales-fetcher.ts
@@ -3,7 +3,7 @@ import { AxiosAdapter } from "./adapter/http/axios.adapter";
 
 
 export const GraciasTotalesFetcher = new AxiosAdapter({
-    baseUrl: 'https://backend-graciastotales.onrender.com/api/', params: {
+    baseUrl: 'https://graciastotales-backend.onrender.com/api/', params: {
 
         //RECORDAR QUE AL CAMBIAR EL BASEURL LLEVA HTTPS, EN LOCALHOST NO.
     }
